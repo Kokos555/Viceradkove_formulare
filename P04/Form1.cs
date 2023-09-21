6d32f33f0af69c95fbf6e6298ca2ef3e1668261d
novotnyv.03 <novotnyv.03@A222-14>
2023-09-21 11:30:04 +0200

    P04

---
+++ P04/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form form2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
